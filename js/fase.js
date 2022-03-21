5a1ad4e61f1aac90220d75875c4447ea9fd7657e
machadogadiel <github@gadiel.dev>
2022-03-21 12:21:35 -0300

    first commit

---
+++ js/fase.js
@@ -0,0 +1,15 @@
+
+function verifyAnswer(correctAnswer) {
+    var answer = prompt("Digite a sua resposta:")
+
+    if (answer.toLowerCase() == 'a'|| answer.toLowerCase() == 'b' || answer.toLowerCase() == 'c' || answer.toLowerCase() == 'd') {
+        if (answer.toLowerCase() !== correctAnswer) {
+            alert("Resposta errada! tente novamente")
+        } else {
+            alert("Reposta correta")
+        }
+        
+    } else {
+        alert("Você precisa responder com uma das 4 opções: A, B, C ou D")
+    }
+}
