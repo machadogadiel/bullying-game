44c2749f4d9dd75f30e90381a073ad46ddeaa214
machadogadiel <github@gadiel.dev>
2022-03-22 21:14:52 -0300

    added js game logic, gameover.html, gamefinished.html and more

--- js/fase.js
+++ js/fase.js
@@ -1,19 +1,44 @@
-
-
-
-
+var currentPhase = window.location.pathname
 
 function verifyAnswer(correctAnswer) {
-    var answer = prompt("Digite a sua resposta:")
+    var num = 1
 
-    if (answer.toLowerCase() == 'a'|| answer.toLowerCase() == 'b' || answer.toLowerCase() == 'c' || answer.toLowerCase() == 'd') {
-        if (answer.toLowerCase() !== correctAnswer) {
-            alert("Resposta errada! tente novamente")
+    while (true) {
+        var answer = prompt("Digite a sua resposta:").toLowerCase()
+
+        if (answer == 'a'|| answer == 'b' || answer == 'c' || answer == 'd') {
+            if (answer == correctAnswer) {
+                alert("Reposta correta")
+                nextPhase()
+                break
+            } else {
+                alert(`Resposta errada! Você tem mais ${num} tentativas`)
+                num++
+            }
+            
         } else {
-            alert("Reposta correta")
+            alert("Você precisa responder com uma das 4 opções: A, B, C ou D")
         }
-        
+
+        console.log(num);
+
+        if (num == 4) {
+            location.href = '../gameover.html'
+            break
+        }
+    }
+
+    
+}
+
+function nextPhase() {
+    if (currentPhase.indexOf("fase1") != -1) {
+        location.href = currentPhase.toString().replace("1", "2")
+    } else if (currentPhase.indexOf("fase2") != -1) {
+        location.href = currentPhase.toString().replace("2", "3")
+    } else if (currentPhase.indexOf("fase3") != -1) {
+        location.href = '../gamefinished.html'
     } else {
-        alert("Você precisa responder com uma das 4 opções: A, B, C ou D")
+        console.log("algo de certo nao esta errado")
     }
 }
