42605c1f85bf6bc3ea67db91ee553a9dfb13646f
machadogadiel <github@gadiel.dev>
2022-03-25 16:42:54 -0300

    translate project to english to improve consistency

--- js/stage.js
+++ js/stage.js
@@ -1,27 +1,26 @@
-
 var currentPhase = window.location.pathname
 
 function verifyAnswer(correctAnswer) {
-    var num = 3
+    var attemps = 3
 
     while (true) {
         var answer = prompt("Digite a sua resposta:").toLowerCase()
 
-        if (answer == 'a'|| answer == 'b' || answer == 'c' || answer == 'd') {
-            if (answer == correctAnswer) {
+        if (answer === 'a'|| answer === 'b' || answer === 'c' || answer === 'd') {
+            if (answer === correctAnswer) {
                 alert("Reposta correta")
-                goToNextPhase()
+                goToNextStage()
                 break
             } else {
-                alert(`Resposta errada! Você tem mais ${num} tentativas`)
-                num--
+                alert(`Resposta errada! Você tem mais ${attemps} tentativas`)
+                attemps--
             }
             
         } else {
             alert("Você precisa responder com uma das 4 opções: A, B, C ou D")
         }
 
-        if (num == 0) {
+        if (attemps === 0) {
             location.href = '../gameover.html'
             break
         }
@@ -30,12 +29,12 @@ function verifyAnswer(correctAnswer) {
     
 }
 
-function goToNextPhase() {
-    if (currentPhase.indexOf("fase1") != -1) {
+function goToNextStage() {
+    if (currentPhase.indexOf("stage1") != -1) {
         location.href = currentPhase.toString().replace("1", "2")
-    } else if (currentPhase.indexOf("fase2") != -1) {
+    } else if (currentPhase.indexOf("stage2") != -1) {
         location.href = currentPhase.toString().replace("2", "3")
-    } else if (currentPhase.indexOf("fase3") != -1) {
+    } else if (currentPhase.indexOf("stage3") != -1) {
         location.href = '../gamefinished.html'
     } else {
         console.log("algo de certo nao esta errado")
